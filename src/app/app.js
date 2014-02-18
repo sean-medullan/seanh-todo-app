ca3ff110450f99543b4cd43f1c47271c5e9c74fe
Sean Hoilett <sean@medullan.com>
2014-02-18 19:25:22 -0400

    Restructured files

--- src/app/app.js
+++ src/app/app.js
@@ -1,24 +1,3 @@
 'use strict';
 
 var todoApp = angular.module("TodoApp", [ 'ngRoute', 'Kinvey']);
-
-todoApp.controller('TodoController', function($scope, KinveyResource){
-
-    $scope.app  = {
-        name: "App Awesome - To Do",
-        todoItems: []
-    };
-
-    $scope.createItem = function(title) {
-        var todoItem = new KinveyResource.todos({
-            title: title,
-            _id: null,
-            isComplete: false,
-            isActive: true,
-            isVisible: true
-        });
-        $scope.app.todoItems.push(todoItem)
-        todoItem.$save();
-    };
-
-});
