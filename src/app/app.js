eddb0b5663e2afa3cbd7a6616de67dfe61dafa82
MaheesIshaq <mahees@medullan.com>
2014-02-19 14:57:42 -0400

    first commit

--- src/app/app.js
+++ src/app/app.js
@@ -1,3 +1,19 @@
 'use strict';
 
-var todoApp = angular.module("TodoApp", [ 'ngRoute', 'Kinvey']);
+var todoApp = angular.module("TodoApp", ['ngRoute', 'Kinvey']);
+
+todoApp.config(['$routeProvider',
+function($routeProvider) {
+	$routeProvider.when('/archived', {
+		templateUrl : 'partials/phone-list.html',
+		controller : 'TodoController'
+	}).when('/trashed', {
+		templateUrl : 'partials/phone-detail.html',
+		controller : 'TodoController'
+	}).when('/all', {
+		templateUrl : 'partials/phone-detail.html',
+		controller : 'TodoController'
+	}).otherwise({
+		redirectTo : '/all'
+	});
+}]); 
--- src/app/todo/controllers/todoController.js
+++ src/app/todo/controllers/todoController.js
@@ -1,10 +1,76 @@
 'use strict';
 
-todoApp.controller('TodoController', function($scope, KinveyResource){
+todoApp.controller('TodoController', function($scope, KinveyResource, TodoService) {
 
-    $scope.app  = {
-        name: "App Awesome - To Do",
-        todoItems: []
-    };
+	var todoItems = TodoService.getTodos();
 
+	$scope.app = {
+		name : "App Awesome - To Do",
+		todoItems : todoItems
+	};
+
+	$scope.addTodo = function(newTodo) {
+		var todo = {
+			name : newTodo.name,
+			completed : false
+		};
+
+		TodoService.addTodo(todo);
+
+		newTodo.name = "";
+	};
+
+	// $scope.updateTodo = function(todo) {
+	// for (var i=0; i < todoItems.length; i++) {
+	// if (todoItems[i].id == todo.id) {
+	// todoItems[i] = todo;
+	// return;
+	// }
+	// };
+	// };
+
+	$scope.removeTodo = function(todo) {
+		TodoService.removeTodo(todo);
+	};
+
+	$scope.completedCount = function(todo) {
+		var completedCount = 0;
+		for (var i = 0; i < todoItems.length; i++) {
+			if (todoItems[i].completed) {
+				completedCount++;
+			}
+		};
+		return completedCount;
+	};
+
+});
+
+todoApp.factory('TodoService', function() {
+	var todoItems = [{
+		id : 1,
+		name : "first todo",
+		completed : false
+	}, {
+		id : 2,
+		name : "second todo",
+		completed : true
+	}];
+
+	return {
+		getTodos : function() {
+			return todoItems;
+		},
+		addTodo : function(todo) {
+			todo.id = todoItems.length;
+			todoItems.push(todo);
+		},
+		removeTodo : function(todo) {
+			for (var i = 0; i < todoItems.length; i++) {
+				if (todoItems[i].id == todo.id) {
+					todoItems.splice(i, 1);
+					return;
+				}
+			};
+		},
+	};
 });
